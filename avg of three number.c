eb686cbd4bf80083f753e27fb5c1b1dd104af049
Pallavi Singh <91817009+Pallavisingh123@users.noreply.github.com>
2021-10-05 15:27:48 +0530

    pallavi singh1

---
+++ avg of three number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+float average(int a,int b,int c);
+int main()
+{
+    int a,b,c ,average;
+    printf("enter the value of a\n");
+    scanf("%d",&a);
+    printf("enter the value of b\n");
+    scanf("%d",&b);
+    printf("enter the value of c\n");
+    scanf("%d",&c);
+    printf("the value of average is %f",average);
+    return 0;
+    float average (int a,int b,int c);
+    {
+
+     float result;
+    result=(float)(a+b+c)/3;
+    return result ;
+    }
+}
+
